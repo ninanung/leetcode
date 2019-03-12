0242d8a5ea0ebec88f548bd4abc48e470b83f25b
Seungje Jun <ninanung@naver.com>
2019-03-12 14:43:54 +0900

    one by one

---
+++ broken_calculator.js
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/broken-calculator/
+
+/**
+ * @param {number} X
+ * @param {number} Y
+ * @return {number}
+ */
+var brokenCalc = function(X, Y) {
+    
+};
--- bulb_switcher_2.js
+++ bulb_switcher_2.js
@@ -6,7 +6,7 @@
  * @return {number}
  */
 var flipAll = function(lights) {
-    const newLights = lights.slice();
+    var newLights = lights.slice();
     for(let i = 0; i < newLights.length; i++) {
         if(newLights[i] === 1) newLights[i] = 0;
         else newLights[i] = 1;
@@ -15,7 +15,7 @@ var flipAll = function(lights) {
 };
 
 var flipEven = function(lights) {
-    const newLights = lights.slice();
+    var newLights = lights.slice();
     for(let i = 0; i < newLights.length; i++) {
         if((i + 1) % 2 === 0) {
             if(newLights[i] === 1) newLights[i] = 0;
@@ -26,7 +26,7 @@ var flipEven = function(lights) {
 };
 
 var flipOdd = function(lights) {
-    const newLights = lights.slice();
+    var newLights = lights.slice();
     for(let i = 0; i < newLights.length; i++) {
         if((i + 1) % 2 !== 0) {
             if(newLights[i] === 1) newLights[i] = 0;
@@ -37,9 +37,9 @@ var flipOdd = function(lights) {
 };
 
 var flipNumber = function(lights) {
-    const newLights = lights.slice();
+    var newLights = lights.slice();
     for(let i = 0; i < newLights.length; i++) {
-        if((i + 1) % 3 === 1 && i + 1 !== 1) {
+        if((i + 1) % 3 === 1) {
             if(newLights[i] === 1) newLights[i] = 0;
             else newLights[i] = 1;
         }
@@ -48,11 +48,11 @@ var flipNumber = function(lights) {
 };
 
 var deleteDuplicatedCase = function(cases) {
-    const newCases = cases.slice();
+    var newCases = cases.slice();
     for(let i = 0; i < newCases.length; i++) {
         for(let j = i + 1; j < newCases.length; j++) {
             let isSame = true;
-            for(let index in newCases[i]) {
+            for(let index = 0; index < newCases[i].length; index++) {
                 if(newCases[i][index] !== newCases[j][index]) {
                     isSame = false;
                 }
@@ -69,29 +69,29 @@ var deleteDuplicatedCase = function(cases) {
 var flipLights = function(n, m) {
     var lights = []
     var cases = []
+    if(m === 0) {
+        return 1;
+    }
     for(let i = 0; i < n; i++) {
         lights.push(1);
     }
     for(let i = 0; i < m; i++) {
+        const casesLength = cases.length
         if(i === 0) {
             cases.push(flipAll(lights));
             cases.push(flipEven(lights));
             cases.push(flipOdd(lights));
             cases.push(flipNumber(lights));
         } else {
-            for(let j = 0; j < cases.length; j++) {
+            for(let j = 0; j < casesLength; j++) {
                 cases.push(flipAll(cases[j]));
                 cases.push(flipEven(cases[j]));
                 cases.push(flipOdd(cases[j]));
                 cases.push(flipNumber(cases[j]));
             }
         }
+        cases.splice(0, casesLength)
         cases = deleteDuplicatedCase(cases).slice();
     }
+    return cases.length;
 };
-
-const test = [1, 1, 1, 1, 1, 1, 1];
-console.log(flipAll(test));
-console.log(flipEven(test));
-console.log(flipOdd(test));
-console.log(flipNumber(test));
