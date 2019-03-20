100912494c73ed7211c8a64b68b97c15e1f17353
Seungje Jun <ninanung@naver.com>
2019-03-20 18:33:52 +0900

    one by one

--- longest_substring_without_repeating_characters.js
+++ longest_substring_without_repeating_characters.js
@@ -4,6 +4,34 @@
  * @param {string} s
  * @return {number}
  */
+var checkDuplicated = function(characters) {
+    const elements = [];
+    let duplicated = false;
+    for(let i = 0; i < characters.length; i++) {
+        if(!elements.length) elements.push(characters[i]);
+        else {
+            let same = false;
+            for(let element of elements) {
+                if(characters[i] === element) {
+                    same = true;
+                    duplicated = true;
+                }
+            }
+            if(!same) elements.push(characters[i]);
+        }
+    }
+    return {
+        elements,
+        duplicated,
+    }
+}
+
 var lengthOfLongestSubstring = function(s) {
-    
+    const checked = checkDuplicated(s);
+    const maxLength = checked.elements.length;
+    if(!checked.duplicated) return s.length;
+    if(maxLength === 1) return maxLength; 
+    else {
+        
+    }
 };
