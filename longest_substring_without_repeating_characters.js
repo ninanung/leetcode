5c5a27a32f68d381f794afa892a1551b86cf2d6b
Seungje Jun <ninanung@naver.com>
2019-03-21 14:08:49 +0900

    one by one

--- longest_substring_without_repeating_characters.js
+++ longest_substring_without_repeating_characters.js
@@ -4,7 +4,7 @@
  * @param {string} s
  * @return {number}
  */
-var checkDuplicated = function(characters) {
+/*var checkDuplicated = function(characters) {
     const elements = [];
     let duplicated = false;
     for(let i = 0; i < characters.length; i++) {
@@ -28,10 +28,28 @@ var checkDuplicated = function(characters) {
 
 var lengthOfLongestSubstring = function(s) {
     const checked = checkDuplicated(s);
-    const maxLength = checked.elements.length;
+    let maxLength = checked.elements.length;
     if(!checked.duplicated) return s.length;
-    if(maxLength === 1) return maxLength; 
+    if(maxLength === 1) return maxLength;
     else {
-        
+        while(maxLength > 0) {
+            for(let i = s.length; i - maxLength >= 0; i--) {
+                if(!checkDuplicated(s.slice(i - maxLength, i)).duplicated) return maxLength;
+            }
+            maxLength--;
+        }
     }
+    return maxLength;
+};*/
+
+var lengthOfLongestSubstring = function(s) {
+    var start = 0, maxLen = 0;
+    var map = new Map();
+    for(var i = 0; i < s.length; i++) {
+        var ch = s[i];
+        if(map.get(ch) >= start) start = map.get(ch) + 1;
+        map.set(ch, i);
+        if(i - start + 1 > maxLen) maxLen = i - start + 1;
+    }
+    return maxLen;
 };
