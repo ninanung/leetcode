dd39a327d60b3e0d340947b036c02c21f598cf5f
Seungje Jun <ninanung@naver.com>
2019-03-25 18:07:09 +0900

    one by one

--- reverse_integer.js
+++ reverse_integer.js
@@ -5,5 +5,14 @@
  * @return {number}
  */
 var reverse = function(x) {
-    
+    let result = 0;
+    while(x) {
+        result *= 10;
+        result += (x % 10);
+        x = parseInt(x/10);
+    }
+    if(Math.pow(2, 31) * -1 > result || result > Math.pow(2, 31) - 1) {
+        return 0;
+    }
+    return result;
 };
---
+++ reverse_integer_atoi.js
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/string-to-integer-atoi/
+
+/**
+ * @param {string} str
+ * @return {number}
+ */
+var myAtoi = function(str) {
+    
+};
