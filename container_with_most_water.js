8c9d0b974287ed8169e874b644e959252fe74e21
Seungje Jun <ninanung@naver.com>
2019-03-26 15:46:47 +0900

    one by one

---
+++ container_with_most_water.js
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/container-with-most-water/
+
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    
+};
--- palindromic_number.js
+++ palindromic_number.js
@@ -5,5 +5,14 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    
+    if(x < 0) return false;
+    let reverse = 0;
+    let original = x;
+    while(x !== 0) {
+        reverse *= 10;
+        reverse += x % 10;
+        x = parseInt(x/10);
+    }
+    if(original === reverse) return true;
+    else return false;
 };
