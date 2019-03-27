1110298eb26c48a9abcd72df4d3b0baf74d53745
Seungje Jun <ninanung@naver.com>
2019-03-27 18:36:01 +0900

    one by one

---
+++ remove_nth_node_from_end_of_list.js
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function(head, n) {
+    
+};
