11c46c802f16ef9b99e794acd231aec259ca738c
Seungje Jun <ninanung@naver.com>
2019-03-28 13:44:26 +0900

    one by one

---
+++ generate_parentheses.js
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/generate-parentheses/
+
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function(n) {
+    
+};
--- valid_parentheses.js
+++ valid_parentheses.js
@@ -5,5 +5,16 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    let stack = '';
+    for(let char of s) {
+        if(char === '\(') stack += '\)';
+        else if(char === '\[') stack += '\]';
+        else if(char === '\{') stack += '\}'
+        else {
+            if(char !== stack[stack.length - 1]) return false;
+            else stack = stack.slice(0, stack.length - 1)
+        }
+    }
+    if(stack.length) return false;
+    return true;
 };
