02fc8b99776dc33825c57d47fbc873fc871af563
Seungje Jun <ninanung@naver.com>
2019-03-28 11:56:45 +0900

    one by one

--- remove_nth_node_from_end_of_list.js
+++ remove_nth_node_from_end_of_list.js
@@ -12,6 +12,20 @@
  * @param {number} n
  * @return {ListNode}
  */
-var removeNthFromEnd = function(head, n) {
-    
+var removeNthFromEnd = function(head, n) {  
+    let count = 0;
+    if(head.next === null || head === null) return null;
+    let linkPointer = {
+        val: null,
+        next: head,
+    }
+    let endPointer = head;
+    while(endPointer !== null) {
+        endPointer = endPointer.next;
+        if(count >= n) linkPointer = linkPointer.next;
+        count++;
+    }
+    if(count === n) return head.next
+    else linkPointer.next = linkPointer.next.next;
+    return head;
 };
---
+++ valid_parentheses.js
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/valid-parentheses/
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    
+};
