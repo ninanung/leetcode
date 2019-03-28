947c5b6a2aae8031b9ae725273647e3af9b13ffd
Seungje Jun <ninanung@naver.com>
2019-03-28 15:11:44 +0900

    one by one

--- generate_parentheses.js
+++ generate_parentheses.js
@@ -4,6 +4,21 @@
  * @param {number} n
  * @return {string[]}
  */
+var recursive = function(string, result, n, stack) {
+    if(!n && stack.length === 0) result.push(string);
+    else {
+        for(let char of '\(\)') {
+            if(char === '\(' && n) recursive(string + '\(', result, n - 1, stack + '\)');
+            if(char === '\)' && stack.length) recursive(string + '\)', result, n, stack.slice(0, stack.length - 1))
+        }
+    }
+};
+
 var generateParenthesis = function(n) {
-    
+    if(!n) return [""];
+    const result = [];
+    let string = '\(';
+    let stack = '\)';
+    recursive(string, result, n - 1, stack);
+    return result;
 };
---
+++ remove_duplicates_from_sorted_array.js
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    
+};
