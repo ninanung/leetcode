5b4c73336fd606bfeef929aaafa7aa69217314fd
Seungje Jun <ninanung@naver.com>
2019-03-29 14:50:42 +0900

    one by one

--- remove_duplicates_from_sorted_array.js
+++ remove_duplicates_from_sorted_array.js
@@ -5,5 +5,13 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    
-};
+    if(!nums.length) return 0;
+    let same = nums[0]
+    for(let num = 1; num < nums.length; num++) {
+        if(same === nums[num]) {
+            nums.splice(num, 1);
+            num--;
+        } else same = nums[num]
+    }
+    return nums.length
+}
---
+++ search_in_rotated_sorted_array.js
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/search-in-rotated-sorted-array/
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    
+};
