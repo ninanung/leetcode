35e7168472f72cacafad6acd167a623eeda5a60e
Seungje Jun <ninanung@naver.com>
2019-03-29 15:43:21 +0900

    one by one

--- search_in_rotated_sorted_array.js
+++ search_in_rotated_sorted_array.js
@@ -6,5 +6,15 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    
+    if(nums[0] > target) {
+        for(let i = nums.length - 1; i >= 0; i--) {
+            if(nums[i] === target) return i;
+        }
+        return -1;
+    } else {
+        for(let i = 0; i < nums.length; i++) {
+            if(nums[i] === target) return i;
+        }
+        return -1;
+    }
 };
