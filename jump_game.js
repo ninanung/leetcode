3fc09df646708e20ef6551e0e693a643af3fe7d0
Seungje Jun <ninanung@naver.com>
2019-04-04 18:49:47 +0900

    one by one

--- jump_game.js
+++ jump_game.js
@@ -25,3 +25,19 @@ var canJump = function(nums) {
     if(recursive(nums, 0, 0, []) === nums.length - 1) return true;
     else return false;
 };*/
+var canJump = function(nums) {
+    if(!nums.length) return false;
+    if(nums.length === 1) return true;
+    if(!nums[0]) return false
+    let standard  = 0;
+    for(let index = nums.length - 1; index >= 0; index--) {
+        if(!standard && nums[index] === 0 && index !== nums.length - 1) {
+            standard = index;
+            continue;
+        }
+        if(standard && nums[index] > standard - index) standard = index;
+        if(standard && nums[standard] !== 0 && nums[index] >= standard - index) standard = index;
+    }
+    if(standard === 0) return true;
+    else return false;
+};
