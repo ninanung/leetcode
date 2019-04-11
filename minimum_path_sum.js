43984add269e3fbd92082c5daeda8f4624d66e55
Seungje Jun <ninanung@naver.com>
2019-04-11 17:20:05 +0900

    one by one

--- minimum_path_sum.js
+++ minimum_path_sum.js
@@ -4,6 +4,38 @@
  * @param {number[][]} grid
  * @return {number}
  */
+var recursive = function(grid, table, x=0, y=0) {
+    if(x > grid[0].length - 1 || y > grid.length - 1) return null;
+    if(x === grid[0].length - 1 && y === grid.length - 1) return grid[y][x];
+    if(table[y][x] !== null) return table[y][x];
+    const value = grid[y][x];
+    const right = recursive(grid, table, x + 1, y);
+    const under = recursive(grid, table, x, y + 1);
+    if(right === null) {
+        table[y][x] = value + under;
+        return value + under;
+    }
+    else if(under === null) {
+        table[y][x] = value + right;
+        return value + right;
+    }
+    else {
+        const small = right < under ? right : under;
+        table[y][x] = value + small;
+        return value + small;
+    }
+}
+
 var minPathSum = function(grid) {
-    
+    if(grid.length === 0) return 0;
+    if(grid[0].length === 0) return 0;
+    let table =[]
+    for(let i = 0; i < grid.length; i++) {
+        let array = [];
+        for(let j = 0; j < grid[0].length; j++) {
+            array.push(null);
+        }
+        table.push(array);
+    }
+    return recursive(grid, table);
 };
