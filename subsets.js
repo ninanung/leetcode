0a689a4e5ac839fcd4223cf11ff22cf97076b67f
Seungje Jun <ninanung@naver.com>
2019-04-15 11:54:13 +0900

    one by one

--- subsets.js
+++ subsets.js
@@ -4,6 +4,20 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
+var recursive = function(nums, index, result, array=[]) {
+    const newArray = array.slice();
+    newArray.push(nums[index])
+    for(let i = index + 1; i < nums.length; i++) {
+        recursive(nums, i, result, newArray);
+    }
+    result.push(newArray);
+};
+
 var subsets = function(nums) {
-    
+    if(nums.length === 0) return [];
+    const result = [[]];
+    for(let index = 0; index < nums.length; index++) {
+        recursive(nums, index, result);
+    }  
+    return result;
 };
---
+++ word_search.js
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/word-search/
+
+/**
+ * @param {character[][]} board
+ * @param {string} word
+ * @return {boolean}
+ */
+var exist = function(board, word) {
+    
+};
