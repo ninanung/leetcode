8bf99798bcc52f422245dbf32d0c6b6c2a63df71
Seungje Jun <ninanung@naver.com>
2019-04-15 15:05:23 +0900

    one by one

---
+++ validate_binary_search_tree.js
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/validate-binary-search-tree/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function(root) {
+    
+};
