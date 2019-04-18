7c0abde822372b9064d9b8ce68b9ce4dab5919fc
Seungje Jun <ninanung@naver.com>
2019-04-18 18:41:49 +0900

    one by one

--- validate_binary_search_tree.js
+++ validate_binary_search_tree.js
@@ -12,5 +12,16 @@
  * @return {boolean}
  */
 var isValidBST = function(root) {
-    
+    let result = true;
+    if(root === null) return true;
+    if(root.left !== null) {
+        if(root.left.val >= root.val) return false;
+        else result = isValidBST(root.left);
+        if(!result) return false;
+    }
+    if(root.right !== null) {
+        if(root.right.val <= root.val) return false;
+        else result = isValidBST(root.right);
+    }
+    return result
 };
