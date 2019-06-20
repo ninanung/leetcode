6031fbfcc3bc21838277f858780167018106e7a8
Seungje Jun <ninanung@naver.com>
2019-06-20 16:11:07 +0900

    one by one

--- populating_next_right_pointers_in_each_node.js
+++ populating_next_right_pointers_in_each_node.js
@@ -12,5 +12,20 @@
  * @return {Node}
  */
 var connect = function(root) {
-    
+    if(root === null) return root;
+    if(root.left === null && root.right === null) return root;
+    const queue = [];
+    queue.push(root);
+    while(queue.length > 0) {
+        const length = queue.length;
+        const nodes = queue.splice(0, length);
+        for(let i = 0; i < length; i++) {
+            let node = nodes.shift()
+            if(nodes.length > 0) node.next = nodes[0];
+            else node.next = null;
+            if(node.left) queue.push(node.left);
+            if(node.right) queue.push(node.right);
+        }
+    }
+    return root;
 };
