037d60d2bb17892be13c7895814a0f6bbe973f16
Seungje Jun <ninanung@naver.com>
2019-07-04 18:54:32 +0900

    one by one

---
+++ copy_list_with_random_pointer.js
@@ -0,0 +1,15 @@
+/**
+ * // Definition for a Node.
+ * function Node(val,next,random) {
+ *    this.val = val;
+ *    this.next = next;
+ *    this.random = random;
+ * };
+ */
+/**
+ * @param {Node} head
+ * @return {Node}
+ */
+var copyRandomList = function(head) {
+    
+};
