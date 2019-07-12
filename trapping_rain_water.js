2c5e69f698a80266b6e869455767d1c850752235
Seungje Jun <ninanung@naver.com>
2019-07-12 18:42:47 +0900

    one by one

---
+++ trapping_rain_water.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var trap = function(height) {
+    function recursive() {
+        
+    }
+    
+    if(height.length < 3) return 0;
+    const stack = [];
+    stack.push(height[0]);
+    
+};
